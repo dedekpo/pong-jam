cdf678410d391127f14d28e8d9c580cfa452baff
dedekpo <dedekpo@gmail.com>
2024-07-19 17:50:37 -0300

    Add more tutorial

--- src/ui/menu/tutorial.tsx
+++ src/ui/menu/tutorial.tsx
@@ -2,10 +2,29 @@ import { useState } from "react";
 
 export default function Tutorial() {
   const [open, setOpen] = useState(true);
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
+
+  const options = [
+    {
+      img: "./assets/tutorial.gif",
+      text: "Move the mouse to move the paddle",
+    },
+    {
+      img: "./assets/power-up.gif",
+      text: "Pick power-up for special effects",
+    },
+    {
+      img: "./assets/hit-center.webp",
+      text: "Hit in the center for more precision",
+    },
+    {
+      img: "./assets/score.webp",
+      text: "Score 7 points to win",
+    },
+  ];
 
   const handleNext = () => {
-    if (step === 2) setOpen(false);
+    if (step === 3) setOpen(false);
     setStep(step + 1);
   };
 
@@ -15,17 +34,9 @@ export default function Tutorial() {
     <div className="absolute top-0 right-0 h-screen w-screen bg-black z-[999] bg-opacity-30 flex items-center justify-center">
       <div className="w-[600px] h-[440px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold mb-1">Tutorial</h1>
-        <p className="text-lg mb-2">
-          {step === 1
-            ? "Move the mouse to move the paddle"
-            : "Pick power-up for special effects"}
-        </p>
+        <p className="text-lg mb-2">{options[step].text}</p>
         <div className="border-2 border-black rounded-lg shadow-lg w-[70%] h-[60%] overflow-hidden">
-          <img
-            src={step === 1 ? "./assets/tutorial.gif" : "./assets/power-up.gif"}
-            className=""
-            alt="Tutorial"
-          />
+          <img src={options[step].img} className="" alt="Tutorial" />
         </div>
         <button
           onClick={handleNext}
